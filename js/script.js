b63e188f56275aa192fd8257fbd69117c91f20a8
mohdmurshi <murshidmuhammed433@gmail.com>
2023-12-30 19:59:20 +0530

    first

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function smoothScroll(target) {
+    document.querySelector(target).scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+// Attach an event listener to the 'About' link
+document.querySelector('nav a[href="#hero"]').addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent default behavior of jumping to the section
+    smoothScroll(this.getAttribute('href')); // Call smoothScroll function
+});
+document.querySelector('nav a[href="#about"]').addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent default behavior of jumping to the section
+    smoothScroll(this.getAttribute('href')); // Call smoothScroll function
+});
+document.querySelector('nav a[href="#whatwedo"]').addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent default behavior of jumping to the section
+    smoothScroll(this.getAttribute('href')); // Call smoothScroll function
+});
+document.querySelector('nav a[href="#portfolio"]').addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent default behavior of jumping to the section
+    smoothScroll(this.getAttribute('href')); // Call smoothScroll function
+});
+document.querySelector('nav a[href="#contact"]').addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent default behavior of jumping to the section
+    smoothScroll(this.getAttribute('href')); // Call smoothScroll function
+});
